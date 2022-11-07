879e4477430d336e07188761d8552dbcb13bc84e
irving vega <irbvega@hotmail.com>
2022-11-07 17:59:07 -0600

    Register feature added

--- app/src/main/java/com/example/dogedex/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/dogedex/auth/AuthViewModel.kt
@@ -1,15 +1,50 @@
 package com.example.dogedex.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dogedex.api.ApiResponseStatus
+import com.example.dogedex.model.Dog
+import com.example.dogedex.model.User
+import kotlinx.coroutines.launch
 
 class AuthViewModel: ViewModel() {
+
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User>
+        get() = _user
+
+    private val _status = MutableLiveData<ApiResponseStatus<User>>()
+    val status: LiveData<ApiResponseStatus<User>>
+        get() = _status
+
+
+    private val authRepository = AuthRepository()
 
     fun signUp(
         email: String,
         password: String,
         confirmPassword: String,
     ){
+        viewModelScope.launch {
+            _status.value = ApiResponseStatus.Loading()
+            handleResponseStatus(
+                authRepository.signUp(
+                    email,
+                    password,
+                    confirmPassword,
+                )
+            )
+        }
+    }
 
+    private fun handleResponseStatus(apiResponseStatus: ApiResponseStatus<User>) {
+        if (apiResponseStatus is ApiResponseStatus.Success) {
+            _user.value = apiResponseStatus.data
+        }
+
+        _status.value = apiResponseStatus
     }
 
 }
--- app/src/main/java/com/example/dogedex/auth/LoginActivity.kt
+++ app/src/main/java/com/example/dogedex/auth/LoginActivity.kt
@@ -1,14 +1,62 @@
 package com.example.dogedex.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
+import com.example.dogedex.MainActivity
 import com.example.dogedex.R
+import com.example.dogedex.api.ApiResponseStatus
+import com.example.dogedex.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity(), LogInFragment.LoginFragmentActions, SignUpFragment.SigUpFragmentActions {
+
+    private val viewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loadingWheel = binding.loadingWheel
+
+        viewModel.status.observe(this) { status ->
+
+            when (status) {
+                is ApiResponseStatus.Loading -> loadingWheel.visibility = View.VISIBLE
+                is ApiResponseStatus.Success -> loadingWheel.visibility = View.GONE
+                is ApiResponseStatus.Error -> {
+                    loadingWheel.visibility = View.GONE
+                    showErrorDialog(status.messageId)
+                }
+            }
+        }
+
+        viewModel.user.observe(this) { user ->
+            if (user != null) {
+                startMainActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+
+    private fun showErrorDialog(messageId: Int) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.there_was_an_error)
+            .setMessage(messageId)
+            .setPositiveButton(android.R.string.ok) {
+                _,_ -> /** Dismiss dialog */
+            }
+            .create()
+            .show()
     }
 
     override fun onRegisterButtonClick() {
@@ -21,6 +69,10 @@ class LoginActivity : AppCompatActivity(), LogInFragment.LoginFragmentActions, S
         password: String,
         confirmPassword: String
     ) {
-        TODO("Not yet implemented")
+        viewModel.signUp(
+            email,
+            password,
+            confirmPassword
+        )
     }
 }
